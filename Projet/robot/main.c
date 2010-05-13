1b615f87ee117bfedf2a64916499eb283e1ee050
Maxime Ripard <maxime.ripard@anandra.org>
2010-05-13 21:10:03 +0000

    Ajout de l'header pour les fonctions de mouvement.

--- Projet/robot/main.c
+++ Projet/robot/main.c
@@ -3,22 +3,27 @@
 #include "base.h"
 #include "structures.h"
 #include "bt.h"
+#include "move.h"
 
 int main(int argc, char* argv) {
   struct robot_struct *robot = malloc(sizeof(robot_struct));
+  unsigned short **map = malloc(MAP_X_SIZE * MAP_Y_SIZE *
+      sizeof(unsigned short));
   robot->mode = MODE_AUTONOMOUS;
 
   while(1) {
+    if(move_is_obstacle())
+      move_handle_obstacle(robot, map);
     bt_check_connect(robot);
     switch(robot->mode) {
       case MODE_AUTONOMOUS:
-        handle_mode_autonomous(robot, map);
+        move_handle_autonomous(robot, map);
         break;
       case MODE_GUIDED:
         if(bt_is_active())
-          handle_mode_guided(robot);
+          move_handle_guided(robot);
         else
-          handle_mode_autonomous(robot, map);
+          move_handle_autonomous(robot, map);
         break;
     }
   }
---
+++ Projet/robot/move.h
@@ -0,0 +1,13 @@
+// Movement functions and constants
+
+#ifndef _LO52_MOVE_H
+#define _LO52_MOVE_H
+
+#include "structures.h"
+
+// Handle an obstacle by notifying the application and escaping from it.
+void move_handle_obstacle(robot_struct*, unsigned short**);
+// Move following the instructions given by the application.
+void handle_mode_guided(robot_struct*);
+// Move in autonomous mode, exploring the field.
+void move_handle_autonomous(robot_struct*, unsigned short**);
