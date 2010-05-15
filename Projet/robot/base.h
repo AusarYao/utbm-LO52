366aca142bf82ae20099501dde4d8d2c49472e47
Maxime Ripard <maxime.ripard@anandra.org>
2010-05-15 14:55:37 +0000

    Include des headers

--- Projet/robot/base.h
+++ Projet/robot/base.h
@@ -2,6 +2,14 @@
 #ifndef _LO52_BASE_H
 #define _LO52_BASE_H
 
+// Include the systems' headers
+#include "base/core.h"
+#include "base/types.h"
+#include "base/drivers/motors.h"
+#include "base/drivers/radar.h"
+#include "base/drivers/sensors.h"
+#include "base/drivers/bt.h"
+
 // Definitions of the different modes used by the robot.
 #define MODE_AUTONOMOUS       1
 #define MODE_GUIDED           2
