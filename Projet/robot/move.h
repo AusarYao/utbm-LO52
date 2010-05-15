a4190faa129d6dfe0ff83a64a645b921c2efe42d
Maxime Ripard <maxime.ripard@anandra.org>
2010-05-15 14:55:32 +0000

    Ajout de constantes.

--- Projet/robot/move.h
+++ Projet/robot/move.h
@@ -5,6 +5,17 @@
 
 #include "structures.h"
 
+#define MOVE_SPEED          80
+#define MOVE_HIGH_SPEED     (SPEED + 10)
+#define MOVE_LOW_SPEED      (SPEED - 10)
+#define MOVE_TURN_SPEED     (SPEED)
+
+#define MOVE_LEFT_MOTOR     2
+#define MOVE_RIGHT_MOTOR    0
+
+#define MOVE_WHEEL_SPACING  116
+#define MOVE_WHEEL_DIAMETER 55
+
 // Handle an obstacle by notifying the application and escaping from it.
 void move_handle_obstacle(robot_struct*, U8**);
 // Move following the instructions given by the application.
