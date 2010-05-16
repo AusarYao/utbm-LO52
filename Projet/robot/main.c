80f349332ff4869278cd5d84c5a6bf9a1ea6e5b8
Maxime Ripard <maxime.ripard@anandra.org>
2010-05-16 14:33:28 +0000

    Pas de malloc ...

--- Projet/robot/main.c
+++ Projet/robot/main.c
@@ -5,26 +5,25 @@
 #include "move.h"
 
 S8 main(void) {
-  struct robot_struct *robot = malloc(sizeof(struct robot_struct));
+  struct robot_struct robot = {MODE_AUTONOMOUS, 0, 0, 0};
   U8 **map = malloc(MAP_X_SIZE * MAP_Y_SIZE * sizeof(U8));
-  robot->mode = MODE_AUTONOMOUS;
 
   while(1) {
     if(move_is_obstacle())
-      move_handle_obstacle(robot, map);
+      move_handle_obstacle(&robot, map);
 
-    bt_check_connect(robot);
+    bt_check_connect(&robot);
 
-    switch(robot->mode) {
+    switch(robot.mode) {
       case MODE_AUTONOMOUS:
-        move_autonomous(robot, map);
+        move_autonomous(&robot, map);
         break;
 
       case MODE_GUIDED:
         if(bt_is_active())
-          move_guided(robot);
+          move_guided(&robot);
         else
-          move_autonomous(robot, map);
+          move_autonomous(&robot, map);
         break;
     }
   }
