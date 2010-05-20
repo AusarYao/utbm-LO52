fbf0598635005d5e2d4fe0c82865e36aefce2ceb
Jeremy Gagnard <gagnard.gagnard@gmail.com>
2010-05-20 06:40:32 +0000

    Ajout des classes Game, Flag, Robot et MapData

---
+++ Projet/application/test_appli/Flag.py
@@ -0,0 +1,18 @@
+# -*- coding:Utf-8 -*-
+
+class Flag(object):
+	"""
+	Simple map with flags coordonnates
+	"""
+	def __init__(self):
+		"""Initialize the map"""
+		self.mapdata =[] 
+
+	def addFlag(self,x,y):
+		"""Add a coordonnate flag"""
+		self.mapdata.append([x,y])
+
+	def delFlag(self,x,y):
+		"""Delete a coordonnate flag"""
+		i = self.mapdata.index([x,y])
+		del self.mapdata[i]
