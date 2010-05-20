045539425023bb39dce615d9316c73627bc68a39
Emilien Colmagne <emilien.colmagne@gmail.com>
2010-05-20 13:39:07 +0000

    changement de bt_message X et Y en 8 bits, ajout de l'envoi de message

--- Projet/application/test_bluetooth/test.py
+++ Projet/application/test_bluetooth/test.py
@@ -1,9 +1,17 @@
 import bluetooth
 
+#adresse mac du nxt
 bd_addr = "00:16:53:09:D7:98"
+
+#port a utiliser avec le nxt
 port = 1
 sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
+#defini la socket non bloquante
+sock.setblocking(0)
+
+#taille des messages envoyes
+MSG_SIZE =4;
 
 def connect():
 	sock.connect((bd_addr, port))
@@ -11,14 +19,27 @@ def connect():
 def recept():
 		data=sock.recv(1024)
 		print data
+		traiteMessage(data)
 
 
 def avance():
-	sock.send("dd")
+	sock.send("dddd")
 
 def droite():
-	sock.send("d\x00")
+	sock.send("d\x00dd")
 
 def gauche():
-	sock.send("\x00d")
+	sock.send("\x00ddd")
 
+#affiche le message et la position du robot
+def traiteMessage(message):
+	for i in range(0, len(message), MSG_SIZE):
+		messageType=ord(message[i])
+		if messageType==0:
+			print 'Mur a droite (' + str(ord(message[i+1])) + ',' + str(ord(message[i+2])) + ')'
+		elif messageType==1:
+			print 'Mur en face (' + str(ord(message[i+1])) + ',' + str(ord(message[i+2]))+ ')'
+		elif messageType==2:
+			print 'Drapeau (' + str(ord(message[i+1])) + ',' + str(ord(message[i+2]))+ ')'
+		elif messageType==3:
+			print 'Position courante (' + str(ord(message[i+1])) + ',' + str(ord(message[i+2]))+ ')'
