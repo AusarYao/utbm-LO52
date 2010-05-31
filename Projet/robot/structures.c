f76799913fad93bf3634a1b56fd12e27439aeea2
Emilien Colmagne <emilien.colmagne@gmail.com>
2010-05-31 18:18:56 +0000

    bugfix

--- Projet/robot/structures.c
+++ Projet/robot/structures.c
@@ -4,6 +4,6 @@
 void struct_map_init(U8 map[MAP_X_SIZE][MAP_Y_SIZE]) {
   int i, j;
   for(i = 0; i < MAP_X_SIZE; i++)
-    for(j = 0; i < MAP_Y_SIZE; j++)
+    for(j = 0; j < MAP_Y_SIZE; j++)
       map[i][j] = 0;
 }
