b2d42a88942e936d079f1b122625d938127bc03b
Emilien Colmagne <emilien.colmagne@gmail.com>
2010-05-31 18:20:09 +0000

    ajout de la gestion des boutons affichage d'infos sur l'ecran du robot

--- Projet/robot/main.c
+++ Projet/robot/main.c
@@ -1,15 +1,48 @@
 #include <stdlib.h>
-
+#include "base/display.h"
 #include "base.h"
+#include "base/core.h"
+#include "base/drivers/systick.h"
+#include "base/drivers/avr.h"
 #include "bt.h"
 #include "move.h"
 #include "sensors.h"
+
+
+//add the possibility to use the buttons
+static void watchdog(void) {
+
+	switch (nx_avr_get_button()) {
+		case BUTTON_CANCEL:
+			nx_core_halt();
+      break;
+		case BUTTON_OK:
+			break;
+		case BUTTON_LEFT:
+			break;
+		case BUTTON_RIGHT:
+			break;
+		case BUTTON_NONE:
+			break;
+  }
+}
+
 
 S8 main(void) {
   struct robot_struct robot = {MODE_AUTONOMOUS, 0, 0, 0};
   U8 map[MAP_X_SIZE][MAP_Y_SIZE];
 
   struct_map_init(map);
+  nx_display_clear();
+	nx_display_cursor_set_pos(0, 0);
+	nx_display_string("Lo52 project\n");
+	nx_display_cursor_set_pos(0, 2);
+
+	//display battery level
+	nx_display_uint(nx_avr_get_battery_voltage());
+	nx_display_cursor_set_pos(4, 2);
+	nx_display_string("/4000\n");
+	nx_systick_install_scheduler(watchdog);
   base_init();
 
   while(1) {
@@ -22,10 +55,11 @@ S8 main(void) {
 
       case MODE_GUIDED:
         if(bt_is_active())
-          move_guided(&robot);
+          move_guided(&robot,&robot);
         else
           move_autonomous(&robot, map);
         break;
     }
   }
+
 }
