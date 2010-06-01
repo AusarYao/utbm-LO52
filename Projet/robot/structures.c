db0fd79f0efc625e06488af1ac29288637075942
Emilien Colmagne <emilien.colmagne@gmail.com>
2010-06-01 13:31:38 +0000

    initilialise les murs exterieurs du terrain

--- Projet/robot/structures.c
+++ Projet/robot/structures.c
@@ -6,4 +6,12 @@ void struct_map_init(U8 map[MAP_X_SIZE][MAP_Y_SIZE]) {
   for(i = 0; i < MAP_X_SIZE; i++)
     for(j = 0; j < MAP_Y_SIZE; j++)
       map[i][j] = 0;
+  for(i = 0; i < MAP_X_SIZE; i++){
+    map[i][0]+=BASE_DOWN;
+    map[i][MAP_Y_SIZE-1]+=BASE_UP;
+  }
+  for(j = 0; j < MAP_Y_SIZE; j++){
+    map[0][j] += BASE_LEFT;
+    map[MAP_X_SIZE-1][j] += BASE_RIGHT;
+  }
 }
