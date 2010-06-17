677a44a6de7f91f54c7267235e8476fab15ec910
Maxime Ripard <maxime.ripard@anandra.org>
2010-06-17 12:00:25 +0000

    Changement d'orientation par defaut

--- Projet/robot/main.c
+++ Projet/robot/main.c
@@ -6,7 +6,7 @@
 #include "base/drivers/sound.h"
 
 S8 main(void) {
-  struct robot_struct robot = {MODE_AUTONOMOUS, 12, 8, BASE_UP, 0};
+  struct robot_struct robot = {MODE_AUTONOMOUS, 12, 8, BASE_RIGHT, 0};
   U8 map[MAP_X_SIZE][MAP_Y_SIZE];
 
   struct_map_init(map);
